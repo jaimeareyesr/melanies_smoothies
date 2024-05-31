40737e840e34ca0fb20a0a803d6122097bee9df2
jaimeareyesr <jaimea.reyesr@gmail.com>
2024-05-31 14:11:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,20 +9,12 @@ st.write(
     """
 )
 
-#import streamlit as st
-#option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"))
-#st.write("Your favorite fruit is:", option)
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
-#session = get_active_session()
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
         'Choose up to 5 ingredients: '
         , my_dataframe
@@ -53,3 +44,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered! ' + name_on_order , icon="✅")
+
+#New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
