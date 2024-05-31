2262f2ac1b6e2e0169f13f15695d08334633278b
jaimeareyesr <jaimea.reyesr@gmail.com>
2024-05-31 14:36:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,20 +23,20 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-	ingredients_string=''
+    ingredients_string=''
 
-	for fruit_chosen in ingredients_list:
-        	ingredients_string += fruit_chosen + ' '    
-		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '    
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+	
+    	st.write(ingredients_string)
+	
+    	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-    		st.write(ingredients_string)
-
-    		my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            		values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
+    st.write(my_insert_stmt)
+    st.stop()
     
     	time_to_insert = st.button('Submit Order')
     
